73d8a57f9ec6cdc451a21da611f2b2cb420613d1
renan-nobrega <renan.nobrega@outlook.com>
2018-03-20 14:51:30 -0300

    Atualização  do código do index pelo da tela de login

--- index.php
+++ index.php
@@ -1,24 +1,54 @@
-<!DOCTYPE html>
+<?php
+$ocorrencia = $_GET["msg"];
+switch ($ocorrencia){
+    case 1:
+        echo ' <div class="col-md-12">
+              <div class="col-md-6"><p class="bg-danger">Digite seu Login</p>
+             </div>
+        </div>';
+
+        break;
+    case 2:
+        echo ' <div class="col-md-12">
+              <div class="col-md-6"><p class="bg-danger">Digite sua senha</p>
+             </div>
+        </div>';
+        break;
+    case 3:
+        echo ' <div class="col-md-12">
+              <div class="col-md-6"><p class="bg-danger">Usuário ou senha incorreta</p>
+             </div>
+        </div>';
+        break;
+
+}
+?>
+
+<div class="col-md-12"> <div class="col-md-6"></div></div>
+
+
 <html lang="pt">
     <head>
-        <meta charset="UTF-8">
-        <title>Aulas - Renan</title>
-
-
-        <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <title>Login</title>
+
     </head>
     <body>
+        <div class="container-fluid">
+            <form action="validar.php" method="get" class="col-md-6">
+                <div class="row">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Login</label>
+                        <input type="text"  name = "login" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Senha</label>
+                        <input type="password"  name = "senha" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Entrar</button>
 
-
-       <div class="jumbotron">
-           <h1>Hello Bootstrap</h1>
-       </div>
-
-
+                </div>
+                </div>
+            </form>
     </body>
 </html>
