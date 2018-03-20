436a68d983a83ddea55fa8db6c5c11706e0d0088
renan-nobrega <renan.nobrega@outlook.com>
2018-03-20 14:52:42 -0300

    Foi inserido o código da tela home que estava em branco.

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+$_SESSION["nome_usuario"];
+
+
+
+
+?>
+<html lang="pt">
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <title>Login</title>
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="jumbotron">
+                <div class="row" class="col-md-12">
+                    <p class="bg-primary"><div class="col-md-9"><h3> <?php echo 'Bem vindo Sr.(a) '.$_SESSION["nome_usuario"]; ?></h3></div></p>
+            </div>
+        </div>
+        </div>
+    </body>
+</html>
