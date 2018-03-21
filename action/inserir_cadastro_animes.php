dfb6421d64c338b0d16a825eabb9bf0b11bd1a35
renan-nobrega <renan.nobrega@outlook.com>
2018-03-21 17:50:28 -0300

    começando a conexão com o bd

---
+++ action/inserir_cadastro_animes.php
@@ -0,0 +1,92 @@
+<?php
+/****************************************************************/
+/*  BIBLIOTECAS                                                 */
+/****************************************************************/
+require_once '../include/db.php';
+require_once '../include/phpLib_cadastro.php';
+
+
+
+/****************************************************************/
+/*  PARAMETROS DE ENTRADA                                       */
+/****************************************************************/
+$cadastro_titulo        = (string)$_POST['cadastro_titulo'];
+$cadastro_ano           = (string)$_POST['cadastro_ano'];
+$cadastro_autor         = (string)$_POST['cadastro_autor'];
+$cadastro_genero        = (string)$_POST['cadastro_genero'];
+$cadastro_cpf           = (string)$_POST['cadastro_cpf'];
+$cadastro_endereco      = (string)$_POST['cadastro_endereco'];
+$cadastro_celular       = (string)$_POST['cadastro_celular'];
+
+
+
+/****************************************************************/
+/*  CONFIGURAÇÕES                                               */
+/****************************************************************/
+$retorno_sucesso        = '../cadastro.php?msg=1';
+$retorno_falha          = '../cadastro.php?msg=0';
+
+
+
+
+/****************************************************************/
+/*  VALIDAR A ENTRADA                                           */
+/****************************************************************/
+// retirando os espaços em branco das laterais do campo
+$cadastro_nome          = trim($cadastro_nome);
+$cadastro_sobrenome     = trim($cadastro_sobrenome);
+$cadastro_login         = trim($cadastro_login);
+$cadastro_senha         = trim($cadastro_senha);
+$cadastro_cpf           = trim($cadastro_cpf);
+$cadastro_endereco      = trim($cadastro_endereco);
+$cadastro_celular       = trim($cadastro_celular);
+
+// tornando mais seguro
+$cadastro_nome          = mysql_real_escape_string($cadastro_nome);
+$cadastro_sobrenome     = mysql_real_escape_string($cadastro_sobrenome);
+$cadastro_login         = mysql_real_escape_string($cadastro_login);
+$cadastro_senha         = mysql_real_escape_string($cadastro_senha);
+$cadastro_cpf           = mysql_real_escape_string($cadastro_cpf);
+$cadastro_endereco      = mysql_real_escape_string($cadastro_endereco);
+$cadastro_celular       = mysql_real_escape_string($cadastro_celular);
+
+// vendo se veio tudo o que obrigatoriamente eu gostaria
+if($cadastro_nome === '') {
+    header('Location: '.$retorno_falha);
+    exit;
+}
+if($cadastro_sobrenome === '') {
+    header('Location: '.$retorno_falha);
+    exit;
+}
+if($cadastro_login === '') {
+    header('Location: '.$retorno_falha);
+    exit;
+}
+if($cadastro_senha === '') {
+    header('Location: '.$retorno_falha);
+    exit;
+}
+if($cadastro_cpf === '') {
+    header('Location: '.$retorno_falha);
+    exit;
+}
+
+
+
+/****************************************************************/
+/*  SCRIPT                                                      */
+/****************************************************************/
+$idUsuario = phpLibCadastro_insert_usuarios_cadastrar_novo_usuario($cadastro_nome, $cadastro_sobrenome, $cadastro_login, $cadastro_senha, $cadastro_cpf, $cadastro_endereco, $cadastro_celular);
+if(!$idUsuario) {
+    header('Location: '.$retorno_falha);
+    exit;
+}
+
+
+
+/****************************************************************/
+/*  FINALIZAR                                                   */
+/****************************************************************/
+header('Location: '.$retorno_sucesso);
+exit;
