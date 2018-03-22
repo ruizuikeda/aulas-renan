e858f88c079b88f5c04d6cf488a0b0755f9be5d0
renan-nobrega <renan.nobrega@outlook.com>
2018-03-22 09:27:19 -0300

    Criado a ligação do php com o banco

---
+++ include/phpLib_cadastro_animes.php
@@ -0,0 +1,20 @@
+<?php
+/********************************************************************/
+/*  Coletânia de funções associadas a cadastro                      */
+/********************************************************************/
+function phpLibCadastro_insert_animes_cadastrar_novo_anime($cadastro_titulo, $cadastro_ano, $cadastro_autor, $cadastro_genero, $cadastro_editora) {
+    $sql = "
+        INSERT INTO animes
+        (titulo, ano, autor, genero, editora)
+        VALUES
+        ('$cadastro_titulo', '$cadastro_ano', '$cadastro_autor', '$cadastro_genero', '$cadastro_editora');
+    ";
+    $result = mysql_query($sql);
+
+    // validando se foi executado com sucesso
+    if(!$result) return false;
+
+    $idAnime = mysql_insert_id();
+
+    return $idAnime;
+}
