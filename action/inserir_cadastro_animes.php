817ab1f01981c34a35d2ac7416c2af259e32ff37
renan-nobrega <renan.nobrega@outlook.com>
2018-03-22 09:32:08 -0300

    atualização do código para inserir na tabela animes

--- action/inserir_cadastro_animes.php
+++ action/inserir_cadastro_animes.php
@@ -3,7 +3,7 @@
 /*  BIBLIOTECAS                                                 */
 /****************************************************************/
 require_once '../include/db.php';
-require_once '../include/phpLib_cadastro.php';
+require_once '../include/phpLib_cadastro_animes.php';
 
 
 
@@ -14,11 +14,7 @@ $cadastro_titulo        = (string)$_POST['cadastro_titulo'];
 $cadastro_ano           = (string)$_POST['cadastro_ano'];
 $cadastro_autor         = (string)$_POST['cadastro_autor'];
 $cadastro_genero        = (string)$_POST['cadastro_genero'];
-$cadastro_cpf           = (string)$_POST['cadastro_cpf'];
-$cadastro_endereco      = (string)$_POST['cadastro_endereco'];
-$cadastro_celular       = (string)$_POST['cadastro_celular'];
-
-
+$cadastro_editora       = (string)$_POST['cadastro_editora'];
 
 /****************************************************************/
 /*  CONFIGURAÇÕES                                               */
@@ -27,47 +23,44 @@ $retorno_sucesso        = '../cadastro.php?msg=1';
 $retorno_falha          = '../cadastro.php?msg=0';
 
 
-
-
 /****************************************************************/
 /*  VALIDAR A ENTRADA                                           */
 /****************************************************************/
 // retirando os espaços em branco das laterais do campo
-$cadastro_nome          = trim($cadastro_nome);
-$cadastro_sobrenome     = trim($cadastro_sobrenome);
-$cadastro_login         = trim($cadastro_login);
-$cadastro_senha         = trim($cadastro_senha);
-$cadastro_cpf           = trim($cadastro_cpf);
-$cadastro_endereco      = trim($cadastro_endereco);
-$cadastro_celular       = trim($cadastro_celular);
+$cadastro_titulo       = trim($cadastro_titulo);
+$cadastro_ano          = trim($cadastro_ano);
+$cadastro_autor        = trim($cadastro_autor);
+$cadastro_genero       = trim($cadastro_genero);
+$cadastro_editora      = trim($cadastro_editora);
+
+
 
 // tornando mais seguro
-$cadastro_nome          = mysql_real_escape_string($cadastro_nome);
-$cadastro_sobrenome     = mysql_real_escape_string($cadastro_sobrenome);
-$cadastro_login         = mysql_real_escape_string($cadastro_login);
-$cadastro_senha         = mysql_real_escape_string($cadastro_senha);
-$cadastro_cpf           = mysql_real_escape_string($cadastro_cpf);
-$cadastro_endereco      = mysql_real_escape_string($cadastro_endereco);
-$cadastro_celular       = mysql_real_escape_string($cadastro_celular);
+$cadastro_titulo      = mysql_real_escape_string($cadastro_titulo);
+$cadastro_ano         = mysql_real_escape_string($cadastro_ano);
+$cadastro_autor       = mysql_real_escape_string($cadastro_autor);
+$cadastro_genero      = mysql_real_escape_string($cadastro_genero);
+$cadastro_editora     = mysql_real_escape_string($cadastro_editora);
+
 
 // vendo se veio tudo o que obrigatoriamente eu gostaria
-if($cadastro_nome === '') {
+if($cadastro_titulo === '') {
     header('Location: '.$retorno_falha);
     exit;
 }
-if($cadastro_sobrenome === '') {
+if($cadastro_ano === '') {
     header('Location: '.$retorno_falha);
     exit;
 }
-if($cadastro_login === '') {
+if($cadastro_autor === '') {
     header('Location: '.$retorno_falha);
     exit;
 }
-if($cadastro_senha === '') {
+if($cadastro_genero === '') {
     header('Location: '.$retorno_falha);
     exit;
 }
-if($cadastro_cpf === '') {
+if($cadastro_editora === '') {
     header('Location: '.$retorno_falha);
     exit;
 }
@@ -77,12 +70,11 @@ if($cadastro_cpf === '') {
 /****************************************************************/
 /*  SCRIPT                                                      */
 /****************************************************************/
-$idUsuario = phpLibCadastro_insert_usuarios_cadastrar_novo_usuario($cadastro_nome, $cadastro_sobrenome, $cadastro_login, $cadastro_senha, $cadastro_cpf, $cadastro_endereco, $cadastro_celular);
-if(!$idUsuario) {
+$idUsuario = phpLibCadastro_insert_animes_cadastrar_novo_anime($cadastro_titulo, $cadastro_ano, $cadastro_autor, $cadastro_genero, $cadastro_editora);
+if(!$idAnime) {
     header('Location: '.$retorno_falha);
     exit;
 }
-
 
 
 /****************************************************************/
