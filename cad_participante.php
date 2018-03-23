016b364770868f1251ba535f3a06bfa2ace5f1e3
renan-nobrega <renan.nobrega@outlook.com>
2018-03-23 11:41:21 -0300

    Criação do meu trabalho da faculdade

---
+++ cad_participante.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <title>Cadastro do Participante</title>
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="cover.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="site-wrapper">
+            <div class="site-wrapper-inner">
+                <div class="cover-container">
+                    <div class="masthead clearfix">
+                        <div class="inner">
+                            <h3 class="masthead-brand">Prova</h3>
+                            <nav>
+                                <ul class="nav masthead-nav">
+                                    <li><a href="index_prova.php">Home</a></li>
+                                    <li><a href="#">Sobre</a></li>
+                                    <li><a href="#">Contatos</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="inner cover">
+                        <h1 class="cover-heading">Primeiro, preencha com seus dados: </h1>
+                    </div>
+                        <form action="action/inserir_cadastro_aluno.php" method="post" class="form-vertical col-md-12">
+                            <div class="form-group col-md-4">
+                                <label class="col-md-12">Nome Completo</label>
+                                <input type="text" class="form-control" name="cadastro_nome" placeholder="Digite o Nome">
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label class="col-md-12">Setor</label>
+                                <input type="text" class="form-control" name="cadastro_rg" placeholder="Digite seu RG">
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label class="col-md-12">Cargo</label>
+                                <input type="text" class="form-control" name="cadastro_cpf" placeholder="Digite seu CPF">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <button type="submit" class="btn btn-primary btn-lg" align="center">Cadastrar</button>
+                            </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ index_prova.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <title>Bem vindo</title>
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="cover.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="site-wrapper">
+            <div class="site-wrapper-inner">
+                <div class="cover-container">
+                    <div class="masthead clearfix">
+                        <div class="inner">
+                            <h3 class="masthead-brand">Bem Vindo!</h3>
+                            <nav>
+                                <ul class="nav masthead-nav">
+                                    <li class="active"><a href="#">Home</a></li>
+                                    <li><a href="#">Sobre</a></li>
+                                    <li><a href="#">Contatos</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="inner cover">
+                        <h1 class="cover-heading">Prova Teórica</h1>
+                        <p class="lead">Olá! Você está pronto para começar o teste com questões relacionadas a matéria dada? Se sim, clica no botão abaixo e boa sorte</p>
+                        <p class="lead">
+                            <a href="#" class="btn btn-lg btn-default">Iniciar Teste</a>
+                        </p>
+                    </div>
+
+                    <div class="mastfoot">
+                        <div class="inner">
+                            <p>Nome do gay aqui do lado direito>> <a href="http://getbootstrap.com">Vinicius Barbosa</a>, by <a href="https://twitter.com/mdo">:)</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
