974b81e9d511f4d3544ba8842c3fc323b9bb5445
renan-nobrega <renan.nobrega@outlook.com>
2018-03-26 17:01:07 -0300

    tentativas de mandar respostas para o banco

--- include/phpLib_inserir_resposta.php
+++ include/phpLib_inserir_resposta.php
@@ -1,18 +1,27 @@
 <?php
 /********************************************************************/
-/*  Coletânia de funções associadas a cadastro                      */
+/*  Coletânia de funções associadas a prova                         */
 /********************************************************************/
+$alternativasSelecionadas      = $_POST;
+
+
+function phpLibVerificar_respostas_certas($alternativasSelecionadas){
+    $resposta1 = $alternativasSelecionadas[0];
+    $resposta2 = $alternativasSelecionadas[1];
+    $resposta3 = $alternativasSelecionadas[2]
+
+
+}
 
 
 
 
-
-function phpLibCadastro_insert_prova_inserir_respostas($cadastro_nome, $alternativasSelecionadas) {
+function phpLibCadastro_insert_prova_inserir_respostas($cadastro_nome, $respostasCertas) {
     $sql = "
         INSERT INTO provas_respondidas
         (nome, respostasCorretas)
         VALUES
-        ('$cadastro_nome', '$alternativasSelecionadas');
+        ('$cadastro_nome', '$respostasCertas');
     ";
     $result = mysql_query($sql);
 
