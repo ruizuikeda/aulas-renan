09b0f1e9327af3217b7fdec931c9ea70be07dab3
renan-nobrega <renan.nobrega@outlook.com>
2018-03-28 18:11:56 -0300

    ta tudo certo:)

--- cad_alunos.php
+++ cad_alunos.php
@@ -1,5 +1,24 @@
 <?php
-$idAluno = $_GET["idAluno"];
+/****************************************************************/
+/*  BIBLIOTECAS                                                 */
+/****************************************************************/
+require_once 'include/db.php';
+require_once 'include/phpLib_select_alunos_cadastrados.php';
+
+$idAluno = $_GET['idAluno'];
+$infoAluno = phpLib_get_aluno($idAluno);
+
+$nome = $infoAluno['nome'];
+$rg = $infoAluno['rg'];
+$cpf = $infoAluno['cpf'];
+$dataNascimento = $infoAluno['dataNascimento'];
+$sexo = $infoAluno['sexo'];
+$curso = $infoAluno['curso'];
+$turno = $infoAluno['turno'];
+
+
+//echo '<pre>'; print_r($curso); exit;
+
 
 ?>
 <html lang="pt">
@@ -38,8 +57,6 @@ $idAluno = $_GET["idAluno"];
                                 <li><a href="index.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Qualquer coisa</font></font></a></li>
                                 <li role="separator" class="divider"></li>
                                 <li class="dropdown-header"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Texto sem link</font></font></li>
-
-
                             </ul>
                         </li>
                     </ul>
@@ -58,51 +75,45 @@ $idAluno = $_GET["idAluno"];
                 <form action="action/inserir_cadastro_aluno.php" method="post" class="form-vertical col-md-12">
                     <div class="form-group col-md-5">
                         <label class="col-md-6">Nome Completo</label>
-                        <input type="text" class="form-control" name="cadastro_nome" placeholder="Digite o Nome">
-                        <?php echo $aluno_nome?>
+                        <input type="text" class="form-control" value="<?php echo $nome?>"name="cadastro_nome" placeholder="Digite o Nome">
                     </div>
 
                     <div class="form-group col-md-5">
                         <label class="col-md-6">RG</label>
-                        <input type="text" class="form-control" name="cadastro_rg" placeholder="Digite seu RG">
+                        <input type="text" class="form-control" value="<?php echo $rg?>" name="cadastro_rg" placeholder="Digite seu RG">
                     </div>
 
                     <div class="form-group col-md-5">
                         <label class="col-md-6">CPF</label>
-                        <input type="text" class="form-control" name="cadastro_cpf" placeholder="Digite seu CPF">
+                        <input type="text" class="form-control" value="<?php echo $cpf?>" name="cadastro_cpf" placeholder="Digite seu CPF">
                     </div>
 
                     <div class="form-group col-md-5">
                         <label class="col-md-6">Data de Nascimento</label>
-                        <input type="date" class="form-control" name="cadastro_dtNascimento" placeholder="Digite a data de Nascimento">
+                        <input type="date" class="form-control" value="<?php echo $dataNascimento?>" name="cadastro_dtNascimento" placeholder="Digite a data de Nascimento">
                     </div>
 
                     <div class= "form-group col-md-5">
                         <label  class="col-md-6">Sexo</label>
                         <select class="form-control col-md-8" size="1" name="cadastro_sexo">
-                            <option></option>
-                            <option value="Masculino">Masculino</option>
-                            <option value="Feminino">Feminino</option>
-                            <option value="Prefiro não Dizer">Prefiro não Dizer</option>
+                            <option value = "Masculino" <?php if ($sexo == "Masculino") echo 'selected'; ?> >Masculino</option>
+                            <option value = "Feminino" <?php if ($sexo == "Feminino") echo 'selected'; ?> >Feminino</option>
+                            <option value = "Prefiro não Dizer" <?php if ($sexo == "Prefiro não Dizer") echo 'selected'; ?> >Prefiro não Dizer</option>
                         </select>
+
                     </div>
                     <div class= "form-group col-md-5">
                         <label  class="col-md-6">Selecione o Curso</label>
-                        <select class="form-control col-md-8" size="1" name="cadastro_curso">
-                            <option></option>
-                            <option value="ADS">ADS</option>
-                            <option value="Banco de Dados">Banco de Dados</option>
-                            <option value="Corte e Custura">Corte e Custura</option>
+                        <select class="form-control col-md-8" size="1"  name="cadastro_curso">
+                            <option value = "ADS" <?php if ($curso == "ADS") echo 'selected'; ?> >ADS</option>                                       <option value = "Banco de Dados" <?php if ($curso == "Banco de Dados") echo 'selected'; ?> >Banco de Dados</option>       <option value = "Corte e Custura" <?php if ($curso == "Corte e Custura") echo 'selected'; ?> >Corte e Custura</option>
                         </select>
                     </div>
 
                     <div class= "form-group col-md-5">
                         <label  class="col-md-6">Período</label>
-                        <select class="form-control col-md-8" size="1" name="cadastro_turno">
-                            <option></option>
-                            <option value="manha">Manhã</option>
-                            <option value="tarde">Tarde</option>
-                            <option value="noite">Noite</option>
+                        <select class="form-control col-md-8" size="1" value="<?php echo $turno?>" name="cadastro_turno">
+                            <option value = "Manhã" <?php if ($curso == "Manhã") echo 'selected'; ?> >Manhã</option>                                 <option value = "Tarde" <?php if ($curso == "Tarde") echo 'selected'; ?> >Tarde</option>
+                            <option value = "Noite" <?php if ($curso == "Noite") echo 'selected'; ?> >Noite</option>
                         </select>
                     </div>
                     <div class="form-group col-md-5">
--- include/phpLib_select_alunos_cadastrados.php
+++ include/phpLib_select_alunos_cadastrados.php
@@ -21,16 +21,15 @@ function phpLib_get_aluno($idAluno) {
     $sql = "
 SELECT *
 FROM alunos
-WHERE id = $idAluno and status = 1
+WHERE id = '$idAluno' AND status = 1
 ";
     $result = mysql_query($sql);
     if(!$result) return array();
     if(mysql_num_rows($result)>0) {
-    return mysql_fetch_assoc($result);
-
-    } else return false;
-
+        while($row = mysql_fetch_assoc($result)) {
+            $r[] = $row;
+        }
+    } else return array();
+    return $r[0];
 }
-
-
 ?>
